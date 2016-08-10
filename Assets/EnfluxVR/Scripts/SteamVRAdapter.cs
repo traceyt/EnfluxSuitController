36314736a50d9c28e5f7114d841d0f715197d654
Matt Brown <mattbrown600@gmail.com>
2016-08-10 00:21:22 -0700

    fixed orientation issue with suit aligning with headset

--- Assets/EnfluxVR/Scripts/SteamVRAdapter.cs
+++ Assets/EnfluxVR/Scripts/SteamVRAdapter.cs
@@ -18,6 +18,8 @@ public class SteamVRAdapter : MonoBehaviour {
             gameObject.transform.SetParent(hmd.transform);
             GameObject.Find("[EnfluxVRHumanoid]").transform.SetParent(gameObject.transform);
             gameObject.transform.position = hmd.transform.position;
+            gameObject.transform.rotation = Quaternion
+                .AngleAxis(hmd.transform.rotation.eulerAngles.y, Vector3.up);
         }
     }
 	
