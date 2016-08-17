1fbc6bce65bdf8ab70c0b8cebe2e2864da92b8ae
Matt Brown <mattbrown600@gmail.com>
2016-08-16 23:22:32 -0700

    updated SteamVRAdapter to deal with waist if no data for waist is used

--- Assets/EnfluxVR/Scripts/SteamVRAdapter.cs
+++ Assets/EnfluxVR/Scripts/SteamVRAdapter.cs
@@ -9,7 +9,8 @@ using UnityEngine;
 public class SteamVRAdapter : MonoBehaviour {
 
     public GameObject hmd;    
-    public Transform waist;
+    public GameObject waist;
+    private Quaternion waistStart;
 
 	// Use this for initialization
 	void Start () {
@@ -20,6 +21,19 @@ public class SteamVRAdapter : MonoBehaviour {
             gameObject.transform.position = hmd.transform.position;
             gameObject.transform.rotation = Quaternion
                 .AngleAxis(hmd.transform.rotation.eulerAngles.y, Vector3.up);
+        }
+
+        if( waist != null)
+        {
+            waistStart = waist.transform.localRotation;
+        }
+    }
+
+    void Update()
+    {
+        if (waist != null && hmd != null)
+        {
+            waist.transform.localRotation = Quaternion.Inverse(hmd.transform.localRotation) * waistStart;
         }
     }
 
