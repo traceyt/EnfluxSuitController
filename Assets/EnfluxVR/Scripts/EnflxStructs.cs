9530fd39c5ec0470cb851854a4a871f354004665
Matt Brown <mattbrown600@gmail.com>
2016-09-05 16:55:18 -0700

    implementing managed worker thread solution

--- Assets/EnfluxVR/Scripts/EnflxStructs.cs
+++ Assets/EnfluxVR/Scripts/EnflxStructs.cs
@@ -4,13 +4,17 @@
 //
 //======================================================================
 
+using System.Runtime.InteropServices;
+
 namespace EnflxStructs
 {
+    [StructLayout(LayoutKind.Sequential)]
     public struct sysmsg
     {
         public string msg;
     }
 
+    [StructLayout(LayoutKind.Sequential)]
     public struct scandata
     {
         public string addr;
@@ -18,6 +22,7 @@ namespace EnflxStructs
         public string name;
     }
 
+    [StructLayout(LayoutKind.Sequential)]
     public struct streamdata
     {
         public string data;
