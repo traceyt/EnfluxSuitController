b4bfc5f11032eab2faa03dfa3b5bf30a2e3163bc
Jordan Tritell <jtritell@indiana.edu>
2016-12-03 13:52:53 -0800

    Fix merge issues in VR scene and VR steam adapter

--- Assets/EnfluxVR/Scripts/SteamVRAdapter.cs
+++ Assets/EnfluxVR/Scripts/SteamVRAdapter.cs
@@ -1,4 +1,4 @@
-﻿e//========= Copyright 2016, Enflux Inc. All rights reserved. ===========
+﻿//========= Copyright 2016, Enflux Inc. All rights reserved. ===========
 //
 // Purpose: Demo adapter for use with SteamVR and HTC Vive
 //
